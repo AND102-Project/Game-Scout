dbe35b44c9fcc24e6019c31640576220c2ea9149
Carlos Sac <98776390+CarlosSacUDC@users.noreply.github.com>
2024-04-23 03:17:34 -0400

    Fix detailed game screen favorites

--- app/src/main/java/com/example/gamescout/favorites/FavoriteFragment.kt
+++ app/src/main/java/com/example/gamescout/favorites/FavoriteFragment.kt
@@ -33,9 +33,9 @@ class FavoriteFragment : Fragment() {
         return binding.root
     }
 
-//    private val repository by lazy {
-//        (requireActivity().application as GameApplication).repository
-//    }
+    private val repository by lazy {
+        (requireActivity().application as GameApplication).repository
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -53,58 +53,32 @@ class FavoriteFragment : Fragment() {
         fetchFavoriteGames()
     }
 
-    // DON' T DELETE - PENDING IMPLEMENTATION
-//    private fun fetchFavoriteGames() {
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            val databaseList = repository.getAll()
-//            databaseList.collect { list ->
-//                val mappedList = list.map { entity ->
-//                    GameItem(
-//                        entity.gameID,
-//                        entity.steamAppID,
-//                        entity.cheapest,
-//                        entity.cheapestDealID,
-//                        entity.external,
-//                        entity.internalName,
-//                        entity.thumb
-//                    )
-//                }
-//                withContext(Dispatchers.Main) {
-//                    favGames.clear()
-//                    favGames.addAll(mappedList)
-//                    adapter.notifyDataSetChanged()
-//                }
-//            }
-//        }
-//    }
-
-
     private fun fetchFavoriteGames() {
-    lifecycleScope.launch(Dispatchers.IO) {
-        (requireActivity().application as GameApplication).db.gameDao().getAll()
-            .collect { databaseList ->
-                favGames.clear()
-                databaseList.map { entity ->
-                    GameItem(
-                        entity.gameID,
-                        entity.steamAppID,
-                        entity.cheapest,
-                        entity.cheapestDealID,
-                        entity.external,
-                        entity.internalName,
-                        entity.thumb
+        lifecycleScope.launch(Dispatchers.IO) {
+            (requireActivity().application as GameApplication).db.gameDao().getAll()
+                .collect { databaseList ->
+                    favGames.clear()
+                    databaseList.map { entity ->
+                        GameItem(
+                            entity.gameID,
+                            entity.steamAppID,
+                            entity.cheapest,
+                            entity.cheapestDealID,
+                            entity.external,
+                            entity.internalName,
+                            entity.thumb
 
-                    )
-                }
-                    .also { mappedList ->
-                        favGames.addAll(mappedList)
-                        launch(Dispatchers.Main) {
-                            adapter.notifyDataSetChanged()
-                        }
+                        )
                     }
-            }
+                        .also { mappedList ->
+                            favGames.addAll(mappedList)
+                            launch(Dispatchers.Main) {
+                                adapter.notifyDataSetChanged()
+                            }
+                        }
+                }
+        }
     }
-}
 
 
     override fun onDestroyView() {
